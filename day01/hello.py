5dc9eb5e9ed6f99ff5ac4b477c165011dc5cb1a0
yexxjx <chenstyle02@gmail.com>
2026-04-15 18:06:17 +0900

    first commit

---
+++ day01/hello.py
@@ -0,0 +1,29 @@
+# p.56
+# vscode 코드에서 python 코드 실행하기 위한 'python' 확장 프로그램 설치
+print("Hello Coding Python")
+
+# Run Python > 실행할 코드에서 shift+enter
+# p.67
+print("Hello Python")
+print("Hello!"*3)
+print("혼자 공부하다 모르면 동영상 강의를 참고하세요!")
+
+# 인터프리터란? 파이썬 코드를 실행할 수 있는 도구 (한 줄 단위 번역) vs 컴파일(파일 단위로 번역)
+# 예) 중국어로 된 편지를 번역할 때 한 번에 번역하면 컴파일 형식, 한 줄씩 번역하면 인터프리터 방식
+
+# 표현식, 식별자, 키워드, 연산자 등 > 문장 (코드) > 프로그램
+
+# p.70
+import keyword
+print(keyword.kwlist)
+
+# p.71 식별자: 이름을 붙인 단어, 변수명/함수명/클래스명 등
+# p.76 출력: print()
+print('# 하나만 출력 합니다.')
+
+print(10,20,30,40,"피카츄")
+
+print() # 빈 줄 출력
+
+# 한 줄 씩 실행: shift+enter
+# 한 번에 실행: 오른쪽 ▷ 클릭
