3475c54060bf761659ba572335e99bdac2407efd
yexxjx <chenstyle02@gmail.com>
2026-04-22 23:00:25 +0900

    0422 23:00

--- day06/practice5.py
+++ day06/practice5.py
@@ -3,50 +3,47 @@
 
 # 문제 1: 성인 인증 함수 (is_adult)
 # 나이(age)를 일반 매개변수로 받아 20세 이상이면 True, 미만이면 False를 반환하는 함수를 작성하시오.
-def is_adult():
-    age=int(input("나이 입력> "))
+def is_adult(age):
     if age>=20:
         return True
     else:
         return False
-print(is_adult())
+input_age=int(input("나이 입력> "))
+print(is_adult(input_age))
 
 # 문제 2: 인사말 출력 함수 (say_hello)
 # 이름(name)과 인사말(message) 두 개의 매개변수를 받는 함수를 작성하시오. 이때 message는 기본 매개변수로 설정하여 값이 입력되지 않으면 "안녕하세요"가 출력되도록 하시오.
 # 호출 예시: say_hello("홍길동") -> 결과: 홍길동님, 안녕하세요.
-def say_hello(name, message):
-    message=input("인사말 입력> ")
-    if message=="":
-        return f"{name}님, 안녕하세요."
-    else:
-        return f"{name}님,{message}"
-print(say_hello("홍길동", message="message"))
+def say_hello(name, message="안녕하세요"):
+    return f"{name}님,{message}."
+print(say_hello("홍길동"))
 
 # 문제 3: 삼각형의 넓이 계산 (get_triangle_area)
 # 밑변(width)과 높이(height)를 매개변수로 받아 삼각형의 넓이를 반환하는 함수를 작성하시오.
-def get_triangle_area():
-    width=int(input("밑변 입력> "))
-    height=int(input("높이 입력> "))
+def get_triangle_area(width, height):
     return ((width*height)/2)
-print(get_triangle_area())
+input_width=int(input("밑변 입력> "))
+input_height=int(input("높이 입력> "))
+print(get_triangle_area(input_width, input_height))
 
 # 문제 4: 입장료 계산 함수 (get_ticket_price)
 # 나이(age)를 매개변수로 받아 입장료를 반환하는 함수를 작성하시오. (8세 미만: 무료, 8~19세: 5,000원, 20세 이상: 10,000원)
-def get_ticket_price():
-    age=int(input("나이 입력> "))
+def get_ticket_price(age):
     if age>=20:
         return ("10,000원")
     if age>=8:
         return ("5,000원")
     else:
         return ("무료")
-print(get_ticket_price())
+input_age=int(input("나이 입력> "))
+print(get_ticket_price(input_age))
 
 # 문제 5: 가변 인자 합계 구하기 (sum_all)
 # 가변 매개변수(*args) 를 사용하여 전달받는 숫자의 개수에 상관없이 모든 숫자의 합계를 반환하는 함수를 작성하시오.
 def sum_all(*args):
     return sum(args)
-print(sum_all())
+input_sum=int(input("숫자 입력> "))
+print(sum_all(input_sum))
 
 # 문제 6: 가장 긴 단어 찾기 (find_longest)
 # 여러 개의 단어를 전개 매개변수로 입력받아 그중 가장 길이가 긴 단어를 반환하는 함수를 작성하시오.
@@ -76,9 +73,8 @@ print(sub_score())
 
 # 문제 10: 상품 결제 시스템 (calculate_payment)
 # 상품 가격(price)과 수량(count)은 일반 매개변수로 받고, 할인율(discount)은 기본 매개변수 0.1(10%)로 설정하시오. (최종 결제 금액 = 가격 * 수량 * (1 - 할인율)) 결과는 정수형으로 반환하시오.
-def calculate_payment():
-    price=int(input("상품 가격> "))
-    count=int(input("수량> "))
-    discount=0.1
-    return ("최종 결제 금액 =", price*count*(1-discount))
-print(calculate_payment())
+def calculate_payment(price, count, discount=0.1):
+    return f"최종 결제 금액 = {price*count*(1-discount)}"
+input_price=int(input("상품 가격> "))
+input_count=int(input("수량> "))
+print(calculate_payment(input_price, input_count))
